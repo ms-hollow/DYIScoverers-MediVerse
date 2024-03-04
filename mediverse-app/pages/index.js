4c9ec96e7d37be3d4f6b16ba12c6826b665a105b
ms-hollow <leonen.abbegail@gmail.com>
2024-03-04 20:54:40 +0800

    added frontend files

---
+++ mediverse-app/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link"
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <h1>Landing Page or Home</h1>
+        <Link href="/register-patient">Register</Link>
+      </div>
+    </>
+  );
+}
---
+++ mediverse-app/pages/register-patient.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link"
+
+const registerPatient = () => {
+    return ( 
+        <>
+        <div>
+            <h1>Register Patient Side</h1>
+            <Link href="/">Home</Link>
+        </div>
+        </>
+    );
+}
+ 
+export default registerPatient;
