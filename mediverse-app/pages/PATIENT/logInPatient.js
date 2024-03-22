9cb71ae63230e1ad06b701006b457fb5e98635c8
J1nWo0 <sherwin.laguidao533@gmail.com>
2024-03-22 21:48:19 +0800

    connect sa metamask ung logInPatient.js

--- mediverse-app/pages/PATIENT/logInPatient.js
+++ mediverse-app/pages/PATIENT/logInPatient.js
@@ -2,15 +2,29 @@ import LandingPageHeader from "@/components/LandingPageHeader";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import web3 from "../../blockchain/web3";
 
 export default function Home() {
+  //connect wallet prompt
+  const connectMetaMask = async () => {
+      try {
+          // Check if MetaMask is installed and prompt user to connect
+          await web3.eth.requestAccounts();
+          console.log("MetaMask connected successfully!");
+          // Perform additional actions after successful connection
+      } catch (error) {
+          console.error('Error connecting MetaMask:', error);
+          // Handle error connecting MetaMask
+      }
+  };
+  
   return (
     <>
       <div> {/* Pass here yung Text na want ilagay sa call-t-action button ng landingPage header */}
           <LandingPageHeader buttonText="REGISTER" />
       </div>
 
-
+      <button onClick={connectMetaMask}>Button</button>
 
         
 
