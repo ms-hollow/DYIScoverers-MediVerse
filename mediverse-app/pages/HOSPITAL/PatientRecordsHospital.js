df87fa195326e83eecbda1ff2f1e85566f2a3a6c
yuraka032 <yurakareserve@gmail.com>
2024-03-28 23:11:23 +0800

    HOSPITAL: Added Patient Records Page

---
+++ mediverse-app/pages/HOSPITAL/PatientRecordsHospital.js
@@ -0,0 +1,59 @@
+import styles from '../../styles/medicalHistory.module.css';
+import Layout from '../../components/HomeSidebarHeader.js'
+import fs from 'fs';
+import path from 'path';
+import Link from 'next/link';
+
+
+export async function getStaticProps() {
+    const filePath = path.join(process.cwd(), 'public/placeHolder/dummyData_PatientRecords.json');
+    const jsonData = fs.readFileSync(filePath, 'utf8');
+    const data = JSON.parse(jsonData);
+
+    return {
+        props: {
+            data
+        }
+    };
+}
+
+const MedicalHistoryPatient = ({ data }) => {
+    const handleAdd = () => {
+        console.log('Button clicked');
+    };
+    
+    return (  
+        <Layout pageName = "Patient Records">
+        <>
+        
+            <div className={styles.container}>
+                <div className={styles.tableHeading}>
+                    <p>Patient Name</p>
+                    <p>Hospital</p>
+                    <p>Admission Date</p>
+                    <p>Discharge Date</p>
+                    <p>Length of Stay</p>
+                </div>
+
+                <div className={styles.dataContainer}>
+                    {data.map(data => (
+                        <Link href="/" key={data.id} className={styles.data}>
+                            <p className={styles.diaAttrb}>{data.name}</p>
+                            <p>{data.hospital}</p>
+                            <p>{data.admissionDate}</p>
+                            <p>{data.dischargeDate}</p>
+                            <p>{data.stayLength}</p>
+                        </Link>
+                    ))}
+                </div>
+
+                <button className={styles.submitButton} onClick={handleAdd}>
+                    <Link href="/">Add Patient</Link>
+                </button>
+            </div>
+        </>
+        </Layout>
+    );
+}
+ 
+export default MedicalHistoryPatient;
