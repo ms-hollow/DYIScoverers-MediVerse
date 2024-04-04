f98ddc513c78d2cb9f8bf45f14be59aa221a0c32
yuraka032 <yurakareserve@gmail.com>
2024-04-04 11:11:29 +0800

    PATIENT: Added Medical History 1

---
+++ mediverse-app/pages/PATIENT/MedicalHistory1Patient.js
@@ -0,0 +1,51 @@
+import styles from '../../styles/medicalHistory.module.css';
+import Layout from '../../components/HomeSidebarHeader.js'
+import fs from 'fs';
+import path from 'path';
+import Link from 'next/link';
+
+
+export async function getStaticProps() {
+    const filePath = path.join(process.cwd(), 'public/placeHolder/dummyData.json');
+    const jsonData = fs.readFileSync(filePath, 'utf8');
+    const data = JSON.parse(jsonData);
+
+    return {
+        props: {
+            data
+        }
+    };
+}
+
+const MedicalHistoryPatient = ({ data }) => {
+    return (  
+        <Layout pageName = "Medical History">
+        <>
+        
+            <div className={styles.container}>
+                <div className={styles.tableHeading}>
+                    <p>Diagnosis</p>
+                    <p>Hospital</p>
+                    <p>Physician</p>
+                    <p>Admission Date</p>
+                    <p>Discharge Date</p>
+                </div>
+
+                <div className={styles.dataContainer}>
+                    {data.map(data => (
+                        <Link href="/" key={data.id} className={styles.data}>
+                            <p className={styles.diaAttrb}>{data.diagnosis}</p>
+                            <p>{data.hospital}</p>
+                            <p>{data.physician}</p>
+                            <p>{data.admissionDate}</p>
+                            <p>{data.dischargeDate}</p>
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        </>
+        </Layout>
+    );
+}
+ 
+export default MedicalHistoryPatient;
