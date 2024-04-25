12f0e9c2f33ff65b19d9e4f0875de444d6866a3c
ms-hollow <leonen.abbegail@gmail.com>
2024-04-25 20:50:56 +0800

    corrected

--- mediverse-app/blockchain/ethers.js
+++ mediverse-app/blockchain/ethers.js
@@ -1,13 +1,13 @@
-import { providers } from 'ethers';
+import { ethers } from 'ethers'; // Correct import
 
 let provider;
 
 if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
     // Modern dapp browsers
-    provider = new providers.Web3Provider(window.ethereum);
+    provider = new ethers.providers.Web3Provider(window.ethereum);
 } else {
     // Fallback to a default provider (e.g., Alchemy)
-    provider = new providers.JsonRpcProvider('https://eth-sepolia.g.alchemy.com/v2/rmGdl7bXf8ZITQxpNjpKc_9RrOnC0wYU');
+    provider = new ethers.providers.JsonRpcProvider('https://eth-sepolia.g.alchemy.com/v2/rmGdl7bXf8ZITQxpNjpKc_9RrOnC0wYU');
 }
 
 export default provider;
