6783d3a86003763fb74080efe3612051ee5b2480
ms-hollow <leonen.abbegail@gmail.com>
2024-04-25 20:46:37 +0800

    modified ethers

--- mediverse-app/blockchain/ethers.js
+++ mediverse-app/blockchain/ethers.js
@@ -1,13 +1,13 @@
-import { ethers } from 'ethers';
+import { providers } from 'ethers';
 
 let provider;
 
 if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
     // Modern dapp browsers
-    provider = new ethers.providers.Web3Provider(window.ethereum);
+    provider = new providers.Web3Provider(window.ethereum);
 } else {
     // Fallback to a default provider (e.g., Alchemy)
-    provider = new ethers.providers.JsonRpcProvider('https://eth-sepolia.g.alchemy.com/v2/rmGdl7bXf8ZITQxpNjpKc_9RrOnC0wYU');
+    provider = new providers.JsonRpcProvider('https://eth-sepolia.g.alchemy.com/v2/rmGdl7bXf8ZITQxpNjpKc_9RrOnC0wYU');
 }
 
 export default provider;
