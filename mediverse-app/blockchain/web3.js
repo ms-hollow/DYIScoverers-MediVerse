827011556c052ba3bca897c6c44ae8eccff2d03e
ms-hollow <leonen.abbegail@gmail.com>
2024-04-25 23:34:10 +0800

    modified mediverse

--- mediverse-app/blockchain/web3.js
+++ mediverse-app/blockchain/web3.js
@@ -1,6 +1,7 @@
 import Web3 from 'web3';
 
 let web3;
+let providerAPI = 'https://eth-sepolia.g.alchemy.com/v2/rmGdl7bXf8ZITQxpNjpKc_9RrOnC0wYU';
 
 if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
     web3 = new Web3(window.ethereum);
@@ -8,7 +9,7 @@ if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
     web3 = new Web3(window.web3.currentProvider);
 } else {
     // Non-dapp browsers or when MetaMask is not available
-    web3 = new Web3('https://eth-sepolia.g.alchemy.com/v2/rmGdl7bXf8ZITQxpNjpKc_9RrOnC0wYU');
+    web3 = new Web3(new Web3.providers.HttpProvider(providerAPI));
 }
 
 export default web3;
