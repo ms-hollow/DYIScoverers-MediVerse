f067c73849adab9134b3ec575ac5812762747718
ms-hollow <leonen.abbegail@gmail.com>
2024-04-26 21:09:50 +0800

    fixing the issue

--- mediverse-app/pages/HOSPITAL/UpdateMedicalHistoryHospital.js
+++ mediverse-app/pages/HOSPITAL/UpdateMedicalHistoryHospital.js
@@ -490,10 +490,10 @@ const UpdateMedicalHistoryHospital = () => {
     
         let newPatientAddr, newPhysician, newDiagnosis, newSymptoms, newTP, newTest, newMedications, newAdmission;
     
+        newPhysician = formData.physician;
         const parsedCurrentMedicalHistory = currentMedicalHistory.map(item => {
-            const [patientAddr, hospitalAddr, physician, diagnosis, signsAndSymptoms, treatmentProcedure, tests, medications, admission, creationDate] = item;
+            const {patientAddr, hospitalAddr, physician, diagnosis, signsAndSymptoms, treatmentProcedure, tests, medications, admission, creationDate} = item;
             newPatientAddr = patientAddr;
-            newPhysician = physician;
             newDiagnosis = diagnosis;
             newSymptoms = signsAndSymptoms;
             newTP = treatmentProcedure;
