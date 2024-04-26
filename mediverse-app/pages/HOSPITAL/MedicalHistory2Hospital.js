e83c5410587e60c3e9f273152fb1afc6fd6a8ad1
ms-hollow <leonen.abbegail@gmail.com>
2024-04-26 15:00:12 +0800

    testing3

--- mediverse-app/pages/HOSPITAL/MedicalHistory2Hospital.js
+++ mediverse-app/pages/HOSPITAL/MedicalHistory2Hospital.js
@@ -114,17 +114,11 @@ const MedicalHistoryHospital = () => {
                 
                 //* So bali ang ginagawa dito is sa list ng medical history ni patient kinukuha yung specific record
                 //* using creation date as key para masearch
-                // const getPatientMedicalHistory = patientRecords.filter(record => {return record.creationDate === creationDate;});
-                // console.log(getPatientMedicalHistory);
-
-                const creationDates = patientRecords.map(record => record.creationDate);
-                console.log(creationDates);
-                const getPatientMedicalHistory = patientRecords.filter((record, index) => {
-                    // Compare each creation date with the target creation date
-                    return creationDates[index] === creationDate;
-                });
+                const getPatientMedicalHistory = patientRecords.filter(record => record.creationDate === creationDate);
+                console.log(getPatientMedicalHistory);
 
                 let physicianName;
+                console.log('Data source:', getPatientMedicalHistory);
                 //* Get yung data sa array na nag equal sa may creationDate
                 const parsedPatientMedicalHistory = getPatientMedicalHistory.map(item => {
                     console.log("Items: ", item)
